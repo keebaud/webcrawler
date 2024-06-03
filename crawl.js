32fda35a95f2ec972b63f68f519f2ac9e35b51cc
Barry Tabrah <keebaud@outlook.com>
2024-06-03 13:51:09 +0100

    This is supposed to be recursing through the pages getting a list of the URLs. However, it seems like pages is never being updated.

--- crawl.js
+++ crawl.js
@@ -28,24 +28,60 @@ function getURLsFromHTML(htmlBody, baseURL) {
     return transformedUrls;
 };
 
-async function crawlPage(baseURL) {
+async function getCurrentHTML(currentURL) {
   try {
-    const response = await fetch(baseURL);
+    const response = await fetch(currentURL);
     if (response.status >= 400) {
-      console.log(`Website response code: ${response.status}`);
+      // console.log(`Website response code: ${response.status}`);
       return;
     }
     const contentType = response.headers.get('content-type');
     if (!contentType || !contentType.includes('text/html')) {
-      console.log('Content Type does not include text/html');
+      // console.log('Content Type does not include text/html');
       return;
     }
 
     const htmlBody = await response.text();
-    console.log(htmlBody);
+    return htmlBody;
   } catch (error) {
-    console.log(`Error: ${error.message}`)
+    // console.log(`Error: ${error.message}`)
   }
+}
+
+async function crawlPage(baseURL, currentURL = baseURL, pages = {}) {
+
+  // Make sure the currentURL is on the same domain as the baseURL
+  const url = new URL(currentURL);
+  // Get Normalized Current URL
+  const normalCurrentURL = normalizeURL(`${url.protocol}//${url.hostname}`);
+  const normalBaseURL = normalizeURL(baseURL);
+  if (normalCurrentURL !== normalBaseURL) {
+    return pages;
+  }
+
+  const normalizedURL = normalizeURL(currentURL);
+  // If pages contains normalCurrentURL, increment pages and return pages
+  // else add to pages and set a count of 1
+  if (pages[normalizedURL]) {
+    pages[normalizedURL]++;
+    return pages
+  } else {
+    pages[normalizedURL] = 1;
+  }
+  
+  // Get the htmlBody text from normalCurrentURL
+  const htmlBody = await getCurrentHTML(currentURL);
+  // If no htmlbody, return pages
+  if (!htmlBody) {
+    return pages;
+  }
+  // Get all valid URLs from htmlBody
+  const validURLS = getURLsFromHTML(htmlBody, baseURL);
+  for (const validURL of validURLS) {
+    pages = crawlPage(baseURL, validURL, pages);
+  }
+  // Return a list of pages
+  return pages
 }
 
 export { normalizeURL, getURLsFromHTML, crawlPage };
